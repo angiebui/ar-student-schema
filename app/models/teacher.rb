7686a562d3be119783df09f7a19eb1227079b870
Apprentice <apprentice@devbootcamp.com>
2013-04-18 18:08:48 -0700

    populate teachers

---
+++ app/models/teacher.rb
@@ -0,0 +1,27 @@
+require_relative '../../db/config'
+
+class PhoneValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless record.phone.scan(/\d/).count >= 10
+      record.errors[attribute] << (options[:message] || "is not a valid phone")
+    end
+  end
+end
+
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless record.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
+class Teacher < ActiveRecord::Base
+  validates :email, :uniqueness => true, :email => true
+  validates :phone, :phone => true
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+end
+
